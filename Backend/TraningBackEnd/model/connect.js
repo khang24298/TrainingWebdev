c7ec25262bc03e0ef22f60e3f9a9c43ec542bdfb
Phong Nguyen <phongnn1998@gmail.com>
2019-03-16 05:40:42 +0700

    Delete

--- Backend/TraningBackEnd/model/connect.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/binh-thanh-management", { useNewUrlParser: true });
--- Backend/TraningBackEnd/model/schema.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var schema = require('./schema/index');
-
-module.exports = {
-  users: mongoose.model('users', schema.users)
-}
